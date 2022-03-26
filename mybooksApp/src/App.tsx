6fb38f4e7adb3121f1b7635711de9c61af1b76c5
Izocel <triangle1232009@hotmail.com>
2022-03-26 14:37:49 -0400

    Passage de fonction App vers ça classe

--- mybooksApp/src/App.tsx
+++ mybooksApp/src/App.tsx
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
 
-export default App;
+            <h1>Bookster®</h1>
+
+            <h3>Liste des livres</h3>
+
+            <table className="BooksTable">
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>Titre</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Fondation</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Les montagnes hallucinÃ©es</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Blade Runner</td>
+                </tr>
+              </tbody>
+            </table>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
+    }
+  }
+
+    export default App;
